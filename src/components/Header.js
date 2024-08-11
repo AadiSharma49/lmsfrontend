c4360ee81ba72bb0a9f8d9646817632b66da133f
Aaditya Sharma <163049815+AadiSharma49@users.noreply.github.com>
2024-08-12 03:39:57 +0530

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,6 +19,7 @@ const Header = () => {
   }
 
   const toggleSidebar = () => {
+
     setIsSidebarOpen(!isSidebarOpen);
   };
 
@@ -28,7 +29,7 @@ const Header = () => {
 
   return (
     <>
-      <div className='w-full bg-gradient-to-r from-blue-500 to-teal-400 h-[130px] shadow-lg flex justify-between items-center p-5 relative'>
+      <div className='w-full bg-gradient-to-r from-slate-900 to-slate-700 flex justify-between items-center p-5 relative'>
         <Link to="/" className='flex justify-center items-center text-white'>
           <img src={logo} className='w-20 h-20 md:w-28 md:h-28 mr-2' alt='logo' />
           <h1 className='text-3xl md:text-5xl font-serif font-bold'>LMS</h1>
@@ -40,40 +41,40 @@ const Header = () => {
           </div>
           {/* Dropdown menu */}
           <div className={`bg-white text-black font-serif w-[300px] absolute rounded-bl-lg justify-start p-5 shadow-lg  right-[-20px] top-[77px] z-10 ${threeDotDropdownOpen ? 'flex flex-col' : 'hidden'}`}>
-            <Link to="/" className='hover:bg-gray-200 p-2 rounded'>Home</Link>
-            <Link to="/about" className='hover:bg-gray-200 p-2 rounded'>About</Link>
-            <Link onClick={toggleServicesDropdownThreeDot} className='hover:bg-gray-200 p-2 rounded flex items-center '>Service <IoMdArrowDropdown /></Link>
+            <Link to="/" className='hover:bg-gray-200 p-2 rounded mx-3 font-sans'>Home</Link>
+            <Link to="/about" className='hover:bg-gray-200 p-2 rounded mx-3 font-sans'>About</Link>
+            <Link onClick={toggleServicesDropdownThreeDot} className='hover:bg-gray-200 p-2 rounded flex items-center mx-3 font-sans'>Service <IoMdArrowDropdown /></Link>
             <Link to="/service1" className={` px-4 py-2 hover:bg-gray-200 ${servicesDropdownOpenThreeDot ? "block":"hidden"} `}>UG Medical Consultancy</Link>
             <Link to="/service2" className={` px-4 py-2 hover:bg-gray-200 ${servicesDropdownOpenThreeDot ? "block":"hidden"}`}>PG Medical Consultancy</Link>
-            <Link to="/psychometrictest" className='hover:bg-gray-200 p-2 rounded'>Psychometric Test</Link>
-            <Link to="/contactform" className='hover:bg-gray-200 p-2 rounded'>Contact Us</Link>
-            <Link to="/login" className='hover:bg-gray-200 p-2 rounded'>Login/Signup</Link>
+            <Link to="/psychometrictest" className='hover:bg-gray-200 p-2 rounded mx-3 font-sans'>Psychometric Test</Link>
+            <Link to="/contactform" className='hover:bg-gray-200 p-2 rounded mx-3 font-sans'>Contact Us</Link>
+            <Link to="/login" className='hover:bg-gray-200 p-2 rounded mx-3 font-sans'>Login/Signup</Link>
           </div>
         </div>
         {/* Main navigation for Desktop */}
-        <div className='hidden  md:flex justify-end lg:justify-around font-serif items-center gap-3 flex-wrap text-white relative text-[25px]'>
-          <Link to="/" className='hover:underline'>Home</Link>
-          <Link to="/about" className='hover:underline'>About</Link>
+        <div className='hidden  md:flex justify-end lg:justify-around font-serif items-center gap-3 flex-wrap text-white relative text-[20px]'>
+          <Link to="/" className='hover:underline hover: transition-all mx-7 font-sans'>Home</Link>
+          <Link to="/about" className='hover:underline hover: transition-all mx-7 font-sans'>About</Link>
           {/* Services dropdown */}
           <div className='relative group'>
-            <div onClick={toggleServicesDropdown} className='hover:underline flex justify-center items-center cursor-pointer'>
+            <div onClick={toggleServicesDropdown} className='hover:underline hover: transition-all mx-7 font-sans flex justify-center items-center cursor-pointer'>
               Services
               <IoMdArrowDropdown />
               <div className={`absolute bg-white top-[100px] left-[9.5px]  lg:top-[77px] lg:left-0 text-black rounded shadow-lg mt-2 ${servicesDropdownOpen ? 'block' : 'hidden'} w-[340px]`}>
-                <Link to="/service1" className='block px-4 py-2 hover:bg-gray-200'>UG Medical Consultancy</Link>
-                <Link to="/service2" className='block px-4 py-2 hover:bg-gray-200'>PG Medical Consultancy</Link>
+                <Link to="/service1" className='block px-4 py-2 hover:bg-gray-200  transition-all font-sans'>UG Medical Consultancy</Link>
+                <Link to="/service2" className='block px-4 py-2 hover:bg-gray-200 transition-all font-sans'>PG Medical Consultancy</Link>
               </div>
             </div>
           </div>
-          <Link to="/psychometrictest" className='hover:underline'>Psychometric Test</Link>
-          <Link to="/contactform" className='hover:underline'>Contact Us</Link>
-          <button onClick={toggleSidebar} className='hover:underline'>Login/Signup</button>
+          <Link to="/psychometrictest" className='hover:underline hover: transition-all  mx-7 font-sans'>Psychometric Test</Link>
+          <Link to="/contactform" className='hover:underline hover: transition-all  mx-7 font-sans'>Contact Us</Link>
+          <button onClick={toggleSidebar} className='hover:underline hover: transition-all mx-7 font-sans'>Login/Signup</button>
         </div>
       </div>
       {/* Sidebar for Login/Signup */}
       {isSidebarOpen && (
         <div className='fixed inset-0 bg-black bg-opacity-50 z-20'>
-          <div className='fixed right-0 top-0 w-64 h-full bg-white shadow-lg p-5'>
+          <div className='fixed right-0 top-0 w-64 h-full backdrop-opacity-55 backdrop-invert bg-white/30  p-5'>
             <div className='flex justify-between items-center mb-4'>
               <h2 className='text-xl font-bold'>Login/Signup</h2>
               <button onClick={toggleSidebar} className='text-xl'>&times;</button>
